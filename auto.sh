fdd41f7f6091e8c5b0c41469a1c76fff928ffff6
yw3 <yw3@abc.com>
2022-01-04 13:57:14 +0800

    2022010413_commit

---
+++ auto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+commit_name="`date +%Y%m%d%H`_commit"
+echo "====git auto push start..."
+echo $commit_name
+git add .
+git commit -m $commit_name
+git push origin
+echo "====git auto push end..."
